a97226e5ec96e657ca2a5d2d537847a987aa2365
Kitpipat Jaritwong <77166960+Gusb3ll@users.noreply.github.com>
2022-04-13 20:04:26 +0700

    [=] refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,7 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-  components: true,
-
   serverMiddleware: [{ path: '/api', handler: '~/server/api' }],
 
-  buildModules: [
-    'nuxt-windicss'
-  ],
-
-  typescript: { shim: false },
-
-  windicss: { analyze: true },
+  buildModules: ['nuxt-windicss'],
 })
